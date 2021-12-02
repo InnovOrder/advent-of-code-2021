b1ae0dc6d6dbd5ea9d7a6017c3f5203fd1033f8c
mkspcd <mkspcd@gmail.com>
2021-12-02 09:14:04 +0100

    Add solution to day 02

--- mkspcd/src/day-01/index.ts
+++ mkspcd/src/day-01/index.ts
@@ -1,12 +1,12 @@
 import path from 'path'
-import { FileName, MeasurementsInput } from '../types'
+import { FileName, MeasurementInput } from '../types'
 import { utils } from '../utils'
 
 const INPUT_FILE: FileName = path.join(__dirname, 'input.txt')
 
-const input: MeasurementsInput = utils.readMeasurementsData(INPUT_FILE)
+const input: MeasurementInput = utils.readMeasurementsData(INPUT_FILE)
 
-function largerMeasurements(input: MeasurementsInput): number {
+function largerMeasurements(input: MeasurementInput): number {
   let count = 0
   
   for (let i = 1; i < input.length; i++)
@@ -15,8 +15,8 @@ function largerMeasurements(input: MeasurementsInput): number {
   return count
 }
 
-function slidingLargerMeasurements(input: MeasurementsInput): number {
-  let slidingInputs: MeasurementsInput = []
+function slidingLargerMeasurements(input: MeasurementInput): number {
+  let slidingInputs: MeasurementInput = []
 
   for (let i = 2; i < input.length; i++)
     slidingInputs.push(input[i] + input[i - 1] + input[i - 2])
--- mkspcd/src/types/index.ts
+++ mkspcd/src/types/index.ts
@@ -1,3 +1,15 @@
 export type FileName = string
 
-export type MeasurementsInput = number[]
+export type MeasurementInput = number[]
+
+export type DirectionInput = {
+  direction: string,
+  distance: number,
+}
+
+export type DirectionInputs = DirectionInput[]
+
+export type Position = {
+  horizontal: number,
+  depth: number,
+}
