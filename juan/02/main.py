00bc5d4057535f78db4859c69d39d77b8845c747
jean1591 <j-rob@live.fr>
2021-12-02 10:33:52 +0100

    dayTwo PartOne

--- juan/02/main.py
+++ juan/02/main.py
@@ -1,4 +1,36 @@
 import data
 
 directions = data.directions
-print(directions)
+
+def formatDirections(directions):
+    formattedDirections = []
+
+    for direction in directions:
+        splittedDirection = direction.split(" ")
+        formattedDirections.append({
+            "direction": splittedDirection[0],
+            "vector": int(splittedDirection[1])
+        })
+
+    return formattedDirections
+
+def partOne(directions):
+    horizontalCount = 0
+    verticalCount = 0
+
+    for direction in directions:
+        if direction["direction"] == "forward":
+            horizontalCount += direction["vector"]
+        elif direction["direction"] == "up":
+            verticalCount -= direction["vector"]
+        elif direction["direction"] == "down":
+            verticalCount += direction["vector"]
+        else:
+            print("I should not be here")
+    
+    return horizontalCount * verticalCount
+
+formattedDirections = formatDirections(directions)
+
+partOneResult = partOne(formattedDirections)
+print(partOneResult)
