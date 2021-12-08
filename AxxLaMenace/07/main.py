6dd91d8ff0b60ed9974ff6e35ef82c0dfb9d2c10
Axel-IO <axel.joigneau@innovorder.fr>
2021-12-08 10:34:03 +0100

    push day 8 with stratégie

--- AxxLaMenace/07/main.py
+++ AxxLaMenace/07/main.py
@@ -1,4 +1,5 @@
 from statistics import median, mean
+from math import ceil, floor
 
 def solve_first_puzzle(crabs):
     goal = int(median(crabs))
@@ -9,7 +10,9 @@ def fuel_cost(dist):
     return dist*(dist+1)//2
 
 def solve_second_puzzle(crabs):
-    goal = int(mean(crabs))
+    med = median(crabs)
+    moy = mean(crabs)
+    goal = ceil(moy) if med>moy else floor(moy)
     fuel = sum([fuel_cost(abs(crab-goal)) for crab in crabs])
     return goal, fuel
 
